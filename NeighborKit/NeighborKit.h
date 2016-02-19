dbb81c7eb924e7432aba5086b78d6699b84b5792
Noah Emmet <noah@noahemmet.com>
2016-02-18 16:32:55 -0800

    Initial Commit

---
+++ NeighborKit/NeighborKit.h
@@ -0,0 +1,19 @@
+//
+//  NeighborKit.h
+//  NeighborKit
+//
+//  Created by Noah Emmet on 2/18/16.
+//  Copyright © 2016 Sticks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NeighborKit.
+FOUNDATION_EXPORT double NeighborKitVersionNumber;
+
+//! Project version string for NeighborKit.
+FOUNDATION_EXPORT const unsigned char NeighborKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeighborKit/PublicHeader.h>
+
+
